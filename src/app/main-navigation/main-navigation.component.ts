3c4decf4e266d8d37073fe59dcc4d0611e96e3ba
MathieuBigare <mathieu@bigare.eu>
2019-06-10 16:25:42 +0200

    feat: added informatiosn screen

--- src/app/main-navigation/main-navigation.component.ts
+++ src/app/main-navigation/main-navigation.component.ts
@@ -1,20 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-main-navigation',
   templateUrl: './main-navigation.component.html',
   styleUrls: ['./main-navigation.component.css']
 })
-export class MainNavigationComponent {
+export class MainNavigationComponent implements OnInit {
+  heroes: Hero[] = [];
+
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private heroService: HeroService
+  ) {}
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
 
 }
