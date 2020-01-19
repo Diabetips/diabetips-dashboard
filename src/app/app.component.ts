32264f7a98331e8d5084b32ba6e75bcc075ef47f
Mathieu BIGARÉ <General Baguettson>
2020-01-19 18:27:05 -0500

    style: minor style corrections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Diabetix 3000';
-  description = 'j\'ai baisé toute vos mères';
+  title = 'Diabetips';
+  description = 'Diabetips\' doctor dashboard';
 }
