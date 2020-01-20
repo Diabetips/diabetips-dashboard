e075d11b9a1129c599034317d7eb40b6c554c457
Mathieu BIGARÉ <General Baguettson>
2020-01-20 07:51:04 -0500

    fix: fixed bad displayed line

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -54,5 +54,18 @@ export class DashboardComponent implements OnInit {
       });
     this.editProfile();
   }
+
+  timestampAsDate(ts: number) {
+    var a = new Date(ts * 1000);
+    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var hour = a.getHours();
+    var min = a.getMinutes();
+    var sec = a.getSeconds();
+    var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+    return time;
+  }
 }
 
